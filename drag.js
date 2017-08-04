a866922a01947c8e569a2715f8772a4197dea745
pubLic-hh <1714793850@qq.com>
2017-08-04 11:14:46 +0800

    change demo1.html and drag.js

--- drag.js
+++ drag.js
@@ -1,8 +1,9 @@
-/*
-* @Author: Administrator
-* @Date:   2017-08-03 18:21:42
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-08-03 18:21:42
-*/
 
-'use strict';
+function (){
+
+	var obj = document.getElementById('div1');
+	var disX = 0;
+	var disY = 0;
+	
+
+}
